0c6b537ee9ae38332933640286ac22143a0a378c
DeJay <contact@dejaydev.com>
2021-05-12 15:08:19 -0500

    Final cut

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 const youtubedl = require('youtube-dl-exec')
 
-const YOUTUBE_REGEX = /(?:youtube\.com\/\S*(?:(?:\/e(?:mbed))?\/|watch\/?\?(?:\S*?&?v\=))|youtu\.be\/)([a-zA-Z0-9_-]{6,11})/g
+const YOUTUBE_REGEX = /(?:https?:\/\/)?(?:www\.)?youtu(?:\.be\/|be.com\/\S*(?:watch|embed)(?:(?:(?=\/[^&\s\?]+(?!\S))\/)|(?:\S*v=|v\/)))([^&\s\?]+)/g
 
 async function getCaptions(url, auto) {
-    if(!YOUTUBE_REGEX.test(url)) {
+    if(!url.match(YOUTUBE_REGEX)) {
         throw new Error('Invalid YouTube URL provided.')
     }
     if(auto == undefined) {
         auto = false
     }
 
-    const res = await youtubedl(url, {
-        //quiet: true,
+    var res = await youtubedl(url, {
         'skip-download': true,
         'list-subs': auto ? null : true,
-        'write-auto-sub': auto ? true : null
+        'write-auto-sub': auto ? true : null,
+        'write-sub': auto ? null : true
     })
+
+    if(res === undefined) {
+        res = ''
+    }
 
     if(res.includes('has no subtitles')) {
         return Promise.reject('The video provided does not have any manually written subtitles. Consider passing auto to download automatic subtitles.')
+    } else {
+        if(auto == false) {
+            await youtubedl(url, {
+                'skip-download': true,
+                'list-subs': null,
+                'write-sub': true
+            })
+        }
     }
 
     return true
 
 }
-
-getCaptions('https://www.youtube.com/watch?v=DYVH5e5x5r0', true).then(res => console.log(res)).catch(err => console.log(err))
 
 module.exports.getCaptions = getCaptions
